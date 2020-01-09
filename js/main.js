1b34c2f39d8b3bd385fa7dc33cf2278f7122ab58
Christian <chris113377@gmail.com>
2020-01-09 09:47:06 -0600

    service worker

--- js/main.js
+++ js/main.js
@@ -4,6 +4,16 @@ let restaurants,
 var newMap
 var markers = []
 
+if ('serviceWorker' in navigator) {
+
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+    .register('./sw.js')
+    .then(res => console.log('Service worker registered!', res))
+    .catch(err => console.log('Not working', err));
+  })
+}
+
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
---
+++ sw.js
@@ -0,0 +1,68 @@
+const staticCacheName = 'v2';
+const filesToCache = [
+  './',
+  './css/styles.css',
+  './index.html',
+  './restaurant.html',
+  './restaurants.json',
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg',
+  './img/4.jpg',
+  './img/5.jpg',
+  './img/6.jpg',
+  './img/7.jpg',
+  './img/8.jpg',
+  './img/9.jpg',
+  './img/10.jpg',
+  './js/dbhelper.js',
+  './js/main.js',
+  './js/restaurant_info.js',
+];
+
+
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open(staticCacheName)
+      .then(cache => cache.addAll(filesToCache))
+      .then(() => self.skipWaiting())
+  )
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== staticCacheName) {
+            console.log('[Service Worker] Removing old cache.', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request)
+    .then(response => {
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
+      } else {
+        return fetch(event.request)
+          .then(res => {
+            return caches.open(staticCacheName)
+              .then(cache => {
+                cache.put(event.request.url, res.clone());
+                return res;
+              })
+          })
+          .catch(err => console.log(err));
+      }
+    })
+  );
+});
