99c6992eb154621500139ccf0ddcee97b316c7b3
Christian <chris113377@gmail.com>
2020-01-09 08:58:06 -0600

    added key

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiY2hyaXMxMTMzNzciLCJhIjoiY2s1NXMxaXplMHhlbTNqbndibDc0bGpkaiJ9.Lig0_l1OZtZXhse_s6v6Yg',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
